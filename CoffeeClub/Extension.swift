085a0b7a38a20958fdfa7e22eca9a3a839eda17f
soobeen27 <youdew27@gmail.com>
2024-07-02 20:00:53 +0900

    modal

---
+++ CoffeeClub/Extension.swift
@@ -0,0 +1,17 @@
+//
+//  Expression.swift
+//  CoffeeClub
+//
+//  Created by Soo Jang on 7/2/24.
+//
+
+import Foundation
+
+extension Int {
+    func numberFormat() -> String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.numberStyle = .decimal
+        guard let formattedNumber = numberFormatter.string(from: NSNumber(value: self)) else { return "" }
+        return formattedNumber + "원"
+    }
+}
