fcacf7956fe2446eec5563fe250bff7b247ed789
soobeen27 <youdew27@gmail.com>
2024-07-02 14:03:26 +0900

    mvc file folder

---
+++ CoffeeClub/Model/Menu.swift
@@ -0,0 +1,10 @@
+//
+//  Menu.swift
+//  CoffeeClub
+//
+//  Created by Soo Jang on 7/2/24.
+//
+
+import Foundation
+
+
---
+++ CoffeeClub/View/CustomMainView.swift
@@ -0,0 +1,20 @@
+//
+//  CustomMainView.swift
+//  CoffeeClub
+//
+//  Created by Soo Jang on 7/2/24.
+//
+
+import UIKit
+
+class CustomMainView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
