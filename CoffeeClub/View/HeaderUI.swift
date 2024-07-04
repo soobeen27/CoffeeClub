71fa609186b473587d818b1d30191220b8acc8e9
유민우 <zx12797@naver.com>
2024-07-04 15:42:14 +0900

    최신버전 카테고리 버튼

--- CoffeeClub/View/HeaderUI.swift
+++ CoffeeClub/View/HeaderUI.swift
@@ -38,18 +38,19 @@ class HeaderUI: UIView {
     
         // 탭 메뉴 버튼들
         let allButton = UIButton(type: .system)
-        allButton.setTitle("All", for: .normal)
+        allButton.setTitle("ALL", for: .normal)
         allButton.setTitleColor(.black, for: .normal)
         allButton.backgroundColor = .white
         allButton.tag = 1
+        allButton.layer.cornerRadius = 5
         allButton.addTarget(categoriseChangeButton, action: #selector(CategoriseChangeButton.buttonTapped(_:)), for: .touchUpInside)
         
         let tabButton1 = UIButton(type: .system)
         tabButton1.setTitle("커피", for: .normal)
         tabButton1.setTitleColor(.black, for: .normal)
-        tabButton1.tag = 1
         tabButton1.backgroundColor = .white
         tabButton1.tag = 2
+        tabButton1.layer.cornerRadius = 5
         tabButton1.addTarget(categoriseChangeButton, action: #selector(CategoriseChangeButton.buttonTapped(_:)), for: .touchUpInside)
         
         let tabButton2 = UIButton(type: .system)
@@ -57,7 +58,7 @@ class HeaderUI: UIView {
         tabButton2.setTitleColor(.black, for: .normal)
         tabButton2.backgroundColor = .white
         tabButton2.tag = 3
-        tabButton2.tag = 2
+        tabButton2.layer.cornerRadius = 5
         tabButton2.addTarget(categoriseChangeButton, action: #selector(CategoriseChangeButton.buttonTapped(_:)), for: .touchUpInside)
         
         let tabButton3 = UIButton(type: .system)
@@ -65,18 +66,18 @@ class HeaderUI: UIView {
         tabButton3.setTitleColor(.black, for: .normal)
         tabButton3.backgroundColor = .white
         tabButton3.tag = 4
-        tabButton3.tag = 3
+        tabButton3.layer.cornerRadius = 5
         tabButton3.addTarget(categoriseChangeButton, action: #selector(CategoriseChangeButton.buttonTapped(_:)), for: .touchUpInside)
         
         // 스택 뷰에 버튼 추가
         let stackView = UIStackView(arrangedSubviews: [allButton, tabButton1, tabButton2, tabButton3])
         stackView.axis = .horizontal
-        stackView.distribution = .equalSpacing
+        stackView.distribution = .fillProportionally
         stackView.alignment = .center
         stackView.spacing = 10
         stackView.backgroundColor = .white
         stackView.translatesAutoresizingMaskIntoConstraints = false
-        stackView.backgroundColor = .white
+        stackView.backgroundColor = UIColor(hex: "#cd2323")
         
         // 헤더 뷰 생성
         let headerView = UIView()
@@ -107,8 +108,8 @@ class HeaderUI: UIView {
             searchBar.leadingAnchor.constraint(equalTo: leadingAnchor),
             searchBar.trailingAnchor.constraint(equalTo: trailingAnchor),
             
-            tabButton1.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 40),
-            tabButton3.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -40),
+             allButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 15),
+             tabButton3.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -15),
             
             // 스택 뷰
             stackView.topAnchor.constraint(equalTo: searchBar.bottomAnchor, constant: 0),
